56c9e3b9f5ad84de0e67db84f69b1aef62859595
SimonPavlin68 <simon.pavlin68@gmail.com>
2016-11-23 14:07:31 +0100

    23.11.2016

---
+++ scipy/interpolate.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import interp1d
+
+n = 5
+x = np.arange(n, dtype=int)
+y = [1,3,2,4,3]
+
+f = interp1d(x, y, kind='quadratic')
+xnew = np.linspace(x[0], x[-1], 100)
+ynew = f(xnew)   # uporabimo interpolacijsko funkcijo dobljeno z `interp1d`
+plt.plot(x, y, 'o', xnew, ynew, '-')
+plt.show()
+
+#x = np.linspace(0, np.pi/2, 4)
+#y = np.sin(x)
+#plt.plot(x, y, 'o-')
+#plt.show()
---
+++ scipy/spl.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import InterpolatedUnivariateSpline
+
+n = 5
+x = np.arange(n, dtype=int)
+y = [1,3,2,4,3]
+plt.plot(x, y, 'ro', label='interpolacijske točke')
+xint = np.linspace(np.min(x), np.max(x), 100)
+spl = InterpolatedUnivariateSpline(x, y, k=3) # poglejte opcije!
+spl1 = spl.derivative(1)
+plt.plot(xint, spl(xint), label='Kubični zlepek');
+plt.plot(xint, spl1(xint), label='Prvi odvod')
+plt.legend(loc='best')
+plt.show()
+print(spl.get_knots())
