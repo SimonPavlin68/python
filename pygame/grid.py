c7739f2f7534fbd15874eb7b9932b646f3e19f27
SimonPavlin68 <simon.pavlin68@gmail.com>
2017-03-23 14:28:44 +0100

    ppon 7.11.2016

--- pygame/grid.py
+++ pygame/grid.py
@@ -1,6 +1,5 @@
 import pygame, math, sys
 import random as rnd
-import numpy as np
 from pygame.locals import *
 
 BLACK = (0, 0, 0)
@@ -16,7 +15,7 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 screen.fill(WHITE)
 
 counter = 0
-#ladja = np.
+Matrix = [[0 for x in range(int(WIDTH/STEP))] for y in range(int(HEIGHT/STEP))]
 
 def init():
     for i in range(0, WIDTH, STEP):
@@ -25,19 +24,63 @@ def init():
         pygame.draw.line(screen, BLACK, [0, j], [WIDTH, j], 1)
     pygame.display.update()
 
-def initboat():
-    xr = rnd.randint(0, 15)
-    yr = rnd.randint(0, 15)
-    smer = rnd.getrandbits(1)
-    print(xr, yr, smer)
+
+def printMatrix():
+    for j in range(int(WIDTH/STEP)):
+        print(Matrix[j])
 
 
-def hit(x, y):
-    pygame.draw.rect(screen, BLUE, [math.floor(x/STEP)*STEP, math.floor(y/STEP)*STEP, STEP, STEP])
+def checkBoat(len, x, y , hor):
+    for i in range(len):
+        if(hor):
+            if (Matrix[x+i][y] > 0):
+                print('colision')
+                return False
+        else:
+            if (Matrix[x][y+i] > 0):
+                print('colision')
+                return False
+    return True
+
+def initboat(length):
+    while True:
+        horizontal = rnd.choice([True, False])
+        if(horizontal):
+            xr = rnd.randint(0, int(WIDTH/STEP)-length-1)
+            yr = rnd.randint(0, int(HEIGHT/STEP)-1)
+        else:
+            xr = rnd.randint(0, int(WIDTH/STEP)-1)
+            yr = rnd.randint(0, int(HEIGHT/STEP)-length-1)
+        print(xr, yr, horizontal)
+        if(checkBoat(length, xr, yr, horizontal)):
+            for i in range(length):
+                if(horizontal):
+                    Matrix[xr+i][yr] = length
+                else:
+                    Matrix[xr][yr+i] = length
+            break;
+
+
+def hit(mx, my):
+    print(mx,my)
+    x = math.floor(mx / STEP)
+    y = math.floor(my / STEP)
+
+    if(Matrix[x][y] > 0):
+        pygame.draw.rect(screen, RED, [x * STEP, y * STEP, STEP, STEP])
+    else :
+        pygame.draw.rect(screen, BLUE, [x * STEP, y * STEP, STEP, STEP])
     pygame.display.update()
 
 init()
-initboat()
+initboat(6)
+initboat(5)
+initboat(4)
+initboat(4)
+initboat(3)
+initboat(3)
+initboat(2)
+printMatrix()
 
 while True:
     for event in pygame.event.get():
