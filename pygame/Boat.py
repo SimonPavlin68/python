6752d1a2419e9f3fb7ac84dde292775cfa290a4f
SimonPavlin68 <simon.pavlin68@gmail.com>
2017-03-24 14:01:22 +0100

    ppon 7.11.2016

---
+++ pygame/Boat.py
@@ -0,0 +1,16 @@
+class Boat:
+
+    def __init__(self, submerged, coordinates):
+        # konstruktor
+        self.submerged = submerged
+        self.coordinates = coordinates
+
+    def set_sirina(self, sirina):
+        self.sirina = sirina
+
+    def hit(self, coord):
+        for c in coordinates:
+            print(c)
+
+    def submerged(self):
+        return self.submerged
--- pygame/grid.py
+++ pygame/grid.py
@@ -7,8 +7,8 @@ RED = (255, 0, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 
-WIDTH = 400
-HEIGHT = 400
+WIDTH = 200
+HEIGHT = 200
 STEP = 20
 RADIUS = 10
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -16,6 +16,11 @@ screen.fill(WHITE)
 
 counter = 0
 Matrix = [[0 for x in range(int(WIDTH/STEP))] for y in range(int(HEIGHT/STEP))]
+boats = []
+
+def boat(submerged, coordinates):
+    submerged = False
+    coordinates = []
 
 def init():
     for i in range(0, WIDTH, STEP):
@@ -33,11 +38,11 @@ def printMatrix():
 def checkBoat(len, x, y , hor):
     for i in range(len):
         if(hor):
-            if (Matrix[x+i][y] > 0):
+            if (Matrix[y][x+i] > 0):
                 print('colision')
                 return False
         else:
-            if (Matrix[x][y+i] > 0):
+            if (Matrix[y+i][x] > 0):
                 print('colision')
                 return False
     return True
@@ -51,13 +56,17 @@ def initboat(length):
         else:
             xr = rnd.randint(0, int(WIDTH/STEP)-1)
             yr = rnd.randint(0, int(HEIGHT/STEP)-length-1)
-        print(xr, yr, horizontal)
+        print(length, xr, yr, horizontal)
         if(checkBoat(length, xr, yr, horizontal)):
+            coordinates = []
             for i in range(length):
                 if(horizontal):
-                    Matrix[xr+i][yr] = length
+                    Matrix[yr][xr+i] = length
+                    coordinates.append([yr, xr+i, 0])
                 else:
-                    Matrix[xr][yr+i] = length
+                    Matrix[yr+i][xr] = length
+                    coordinates.append([yr+i, xr, 0])
+            boats.append(boat(False, coordinates))
             break;
 
 
@@ -66,10 +75,10 @@ def hit(mx, my):
     x = math.floor(mx / STEP)
     y = math.floor(my / STEP)
 
-    if(Matrix[x][y] > 0):
-        pygame.draw.rect(screen, RED, [x * STEP, y * STEP, STEP, STEP])
-    else :
-        pygame.draw.rect(screen, BLUE, [x * STEP, y * STEP, STEP, STEP])
+    color = BLUE
+    if(Matrix[y][x] > 0):
+        color = RED
+    pygame.draw.rect(screen, color, [x * STEP, y * STEP, STEP, STEP])
     pygame.display.update()
 
 init()
@@ -81,13 +90,14 @@ initboat(3)
 initboat(3)
 initboat(2)
 printMatrix()
+print(boats)
 
 while True:
     for event in pygame.event.get():
-        if event.type == QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        if event.type == MOUSEBUTTONDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN:
             posx, posy = pygame.mouse.get_pos()
             counter += 1
             print(counter)
